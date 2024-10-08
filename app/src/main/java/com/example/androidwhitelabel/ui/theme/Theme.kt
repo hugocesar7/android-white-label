86d8b12dafa69d45a347f0315255b1fd36739089
Hugo Ribeiro <hugocesar.ribeiro@gmail.com>
2024-10-08 11:41:31 -0300

    Depois do build

--- app/src/main/java/com/example/androidwhitelabel/ui/theme/Theme.kt
+++ app/src/main/java/com/example/androidwhitelabel/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun AndroidwhitelabelTheme(
+fun AndroidWhiteLabelTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
